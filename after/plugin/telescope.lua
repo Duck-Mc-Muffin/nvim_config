9a3557048fd084e7bc2624a3b609083b02f1c210
Steven Pauls <stevenpauls@gmx.de>
2024-04-21 20:55:52 +0200

    Added 'find marks' Telescope-mapping;

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -5,6 +5,7 @@ end, {desc = "Telescope find files"})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {desc = "Telescope find with live grep (cooler)"})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {desc = "Telescope find in buffers"})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {desc = "Telescope find help tags"})
+vim.keymap.set('n', '<leader>fm', builtin.marks, {desc = "Telescope find marks"})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {desc = "Telescope find in git files"})
 
 vim.keymap.set('n', '<leader>fvg', function()
