2be248f466e25bb48fe164fe4dc8e36894f2f3ac
Steven Pauls <stevenpauls@gmx.de>
2024-06-17 19:23:26 +0200

    Added :Where command to open the current file in mini-files without anchoring

--- plugin/mini-files.lua
+++ plugin/mini-files.lua
@@ -9,3 +9,8 @@ mini_files.setup({
 vim.keymap.set('n', '<leader>q', function()
     mini_files.open()
 end, {desc = "Open mini.file-explorer"})
+
+-- Open the current file in mini-files but don't set the anchor directory
+vim.api.nvim_create_user_command('Where', function(_)
+    MiniFiles.open(vim.api.nvim_buf_get_name(0), false)
+end, {})
