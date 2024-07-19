df1ab3e0a64c87d7c88a16e5a51406708e317ae2
Steven Pauls <stevenpauls@gmx.de>
2024-07-19 17:32:40 +0200

    Added visual mode to code action mapping

--- plugin/lsp-zero.lua
+++ plugin/lsp-zero.lua
@@ -5,7 +5,7 @@ lsp_zero.on_attach(function(_, bufnr)
     local opts = {buffer = bufnr, remap = false}
     vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
     vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set({"n", "v"}, "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
     vim.keymap.set("n", "<leader>vrq", function() vim.lsp.buf.references() end, opts)
     vim.keymap.set("n", "<leader>vrr", function() require('telescope.builtin').lsp_references() end, opts)
     vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
