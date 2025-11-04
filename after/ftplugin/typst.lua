8d066351326d21ff834ed2be67d55affbcdaf0e0
Steven Pauls <dev@stevenpauls.de>
2025-11-04 09:33:17 +0100

    Fixed Typst config

---
+++ after/ftplugin/typst.lua
@@ -0,0 +1,14 @@
+
+-- Activate spell checking
+vim.opt_local.spell = true
+vim.opt_local.spelllang = 'de_20,en_us'
+
+-- Misc
+-- vim.opt_local.commentstring = '//%s'
+
+-- Tabs
+vim.opt_local.tabstop = 4
+vim.opt_local.shiftwidth = 4
+vim.opt_local.expandtab = true
+vim.opt_local.softtabstop = 4
+
--- plugin/typst.lua
+++ plugin/typst.lua
@@ -1,7 +1,9 @@
 require('typst-preview').setup(
 {
-    dependencies_bin = {
-        ['tinymist'] = 'tinymist',
-        ['websocat'] = nil
-    },
+    --dependencies_bin = {
+    --    ['tinymist'] = 'tinymist',
+    --    ['websocat'] = nil
+    --},
+    port = 8000,
+    get_root = function (_main_file) return vim.fn.getcwd() end,
 })
